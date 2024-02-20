25a185c9311cd1fde1b625855de9df9079cc263b
DawnGate <lam99quyen@gmail.com>
2024-02-20 15:41:38 +1200

    micro-component: done login form

--- micro-components/form/index.js
+++ micro-components/form/index.js
@@ -1,1 +1,25 @@
-console.log("Hello");
+const pwInput = document.getElementById("password");
+
+const iconEye = document.getElementById("iconEyeSm");
+
+const iconEyeOff = document.getElementById("iconEyeOffSm");
+
+iconEye.addEventListener("click", () => {
+  hidePassword();
+});
+
+iconEyeOff.addEventListener("click", () => {
+  showPassword();
+});
+
+const showPassword = () => {
+  pwInput.setAttribute("type", "text");
+  iconEye.style = "display: block;";
+  iconEyeOff.style = "display: none;";
+};
+
+const hidePassword = () => {
+  pwInput.setAttribute("type", "password");
+  iconEye.style = "display: none;";
+  iconEyeOff.style = "display: block;";
+};
