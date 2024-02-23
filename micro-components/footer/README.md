3580b54e654e9ed61b733a5e63a2c255ea062110
DawnGate <lam99quyen@gmail.com>
2024-02-23 20:41:50 +1200

    micro-component feat: footer version 0.3 action in language and currency box

--- micro-components/footer/README.md
+++ micro-components/footer/README.md
@@ -16,6 +16,12 @@ We will create a simple footer with html, css, js from the example in internet
   ![Version 0.2](./demo/version_0.2.png "Version 0.2 demo")
   _(image) Version 0.2_
 
-- Version 0.3: Modal and action (for settings tab)
+- Version 0.3: Modal and action (for settings tab - change language and currency -> only combo box this will not change the current view or may be in the future later)
 
-- Version 0.4: Theme color change and auto with system color scheme
+![Version 0.3](./demo/version_0.3.2.png "Version 0.3 demo")
+_(image) Version 0.3 Mobile view_
+
+![Version 0.3](./demo/version_0.3.1.png "Version 0.3 demo")
+_(image) Version 0.3 Laptop view_
+
+- Version 0.4: Theme color change and auto with system color scheme (change theme button)
--- micro-components/footer/index.js
+++ micro-components/footer/index.js
@@ -1,8 +1,20 @@
+// utils function
+let debounceTimeOut;
+const debounce = (func, timeOut) => {
+  if (debounceTimeOut) {
+    clearTimeout(debounceTimeOut);
+  }
+  debounceTimeOut = setTimeout(() => {
+    func();
+  }, timeOut ?? 500);
+};
+
+//
 const footerSubNavs = document.querySelectorAll(
   ".footer-navlist-sub .footer-navlist-title"
 );
 
-for (elem of footerSubNavs) {
+for (let elem of footerSubNavs) {
   elem.addEventListener("click", () => {
     const parent = elem.parentNode;
     if (parent.className.includes("open")) {
@@ -12,3 +24,128 @@ for (elem of footerSubNavs) {
     }
   });
 }
+
+const bodyElem = document.querySelector("body");
+const lngAndCurrencyDrawer = document.querySelector(
+  ".mobile-setting-drawer.draw-mask"
+);
+const btnCloseLngAndCurrencyDrawer = document.getElementById(
+  "lngAndCurrent-drawer-close-btn"
+);
+const settingLanguageBtn = document.getElementById("setting-language");
+const settingCurrencyBtn = document.getElementById("setting-currency");
+
+const showLngAndCurrencyDrawer = () => {
+  bodyElem.classList.add("open-drawer");
+  lngAndCurrencyDrawer.classList.add("open");
+};
+
+const hideLngAndCurrencyDrawer = () => {
+  document.querySelector("body").classList.remove("open-drawer");
+  lngAndCurrencyDrawer.classList.remove("open");
+};
+
+const eventShowLngAndCurrencyDrawer = () => {
+  showLngAndCurrencyDrawer();
+};
+
+const invokeSettingForMobile = () => {
+  settingCurrencyBtn.addEventListener("click", eventShowLngAndCurrencyDrawer);
+
+  settingLanguageBtn.addEventListener("click", eventShowLngAndCurrencyDrawer);
+};
+
+const cleanInvokeSettingForMobile = () => {
+  settingCurrencyBtn.removeEventListener(
+    "click",
+    eventShowLngAndCurrencyDrawer
+  );
+
+  settingLanguageBtn.removeEventListener(
+    "click",
+    eventShowLngAndCurrencyDrawer
+  );
+};
+
+btnCloseLngAndCurrencyDrawer.addEventListener("click", () => {
+  hideLngAndCurrencyDrawer();
+});
+
+const eventAddActiveForCurrencyParent = () => {
+  settingCurrencyBtn.parentNode.classList.add("active");
+};
+
+const eventRemoveActiveForCurrencyParent = () => {
+  settingCurrencyBtn.parentNode.classList.remove("active");
+};
+
+const eventAddActiveForLanguageParent = () => {
+  settingLanguageBtn.parentNode.classList.add("active");
+};
+
+const eventRemoveActiveForLanguageParent = () => {
+  settingLanguageBtn.parentNode.classList.remove("active");
+};
+
+const invokeSettingForLaptop = () => {
+  settingCurrencyBtn.addEventListener(
+    "mouseenter",
+    eventAddActiveForCurrencyParent
+  );
+
+  settingCurrencyBtn.parentNode.addEventListener(
+    "mouseleave",
+    eventRemoveActiveForCurrencyParent
+  );
+
+  settingLanguageBtn.addEventListener(
+    "mouseenter",
+    eventAddActiveForLanguageParent
+  );
+
+  settingLanguageBtn.parentNode.addEventListener(
+    "mouseleave",
+    eventRemoveActiveForLanguageParent
+  );
+};
+
+const cleanInvokeSettingForLaptop = () => {
+  settingCurrencyBtn.removeEventListener(
+    "mouseenter",
+    eventAddActiveForCurrencyParent
+  );
+
+  settingCurrencyBtn.parentNode.removeEventListener(
+    "mouseleave",
+    eventRemoveActiveForCurrencyParent
+  );
+
+  settingLanguageBtn.removeEventListener(
+    "mouseenter",
+    eventAddActiveForLanguageParent
+  );
+
+  settingLanguageBtn.parentNode.removeEventListener(
+    "mouseleave",
+    eventRemoveActiveForLanguageParent
+  );
+};
+
+const addSettingsAction = () => {
+  cleanInvokeSettingForMobile();
+  cleanInvokeSettingForLaptop();
+  if (window.innerWidth < 768) {
+    invokeSettingForMobile();
+  } else {
+    hideLngAndCurrencyDrawer();
+    invokeSettingForLaptop();
+  }
+};
+
+addSettingsAction();
+
+window.addEventListener("resize", () => {
+  debounce(() => {
+    addSettingsAction();
+  }, 500);
+});
