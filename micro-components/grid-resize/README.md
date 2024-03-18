70a462ff73309cd99ce750a25e20cd043133efbb
DawnGate <lam99quyen@gmail.com>
2024-03-18 22:08:24 +1200

    micro-components feat: update new component

---
+++ micro-components/grid-resize/README.md
@@ -0,0 +1,23 @@
+# Grid resizable container - micro-component
+
+ideas from [site]
+
+# Introduce
+
+We will create a simple Resizable grid with html, css, js from the example in internet
+
+1. The current resizable box (the box can resize from both horizontal and vertical is pending -> time to do more than using library) [ - not done, maybe we comeback later]
+
+2. The grid item inner the box can recalculate and give the max possible items can inner the box with spacing [X]
+
+We will focus on how render many as possible box with video aspect ratio ( For video conference box :D)
+
+# Version history:
+
+- Version 0.1: View when have multiple
+  ![Version 0.1](./demo/version_0.1.png "Version 0.1 demo")
+  _(image) Version 0.1_
+
+- Version 0.1: View when have 1 row, or column
+  ![Version 0.1](./demo/version_0.2.png "Version 0.2 demo")
+  _(image) Version 0.2_
---
+++ micro-components/grid-resize/index.js
@@ -0,0 +1,150 @@
+const MainBox = () => {
+  const handleDrag = (e) => {
+    console.log(e);
+  };
+
+  return (
+    <div className="resizable-box">
+      <div
+        className="resizable-straight rs-horizontal left"
+        onDragStart={handleDrag}
+      ></div>
+      <div className="resizable-straight rs-horizontal right"></div>
+      <div className="resizable-straight rs-vertical top"></div>
+      <div className="resizable-straight rs-vertical bottom"></div>
+      <div>This will implement later</div>
+    </div>
+  );
+};
+
+const GridItem = () => {
+  return <div className="grid-item"></div>;
+};
+
+const maxItemWidth = 178;
+const minItemWidth = 100;
+
+function calculateMaxItemInBoxWithSpacing(containerWidth, itemWidth, spacing) {
+  const availableWidth = containerWidth - itemWidth;
+
+  if (availableWidth < 0) return 0;
+
+  const maxItem = Math.floor(availableWidth / (itemWidth + spacing));
+
+  return maxItem + 1;
+}
+
+function calculateNumberOfItemsInGrid(containerWidth, containerHeight) {
+  const itemSpacing = 16;
+  const itemAspectRatio = 16 / 9;
+
+  if (
+    containerWidth < minItemWidth ||
+    containerHeight < minItemWidth / itemAspectRatio
+  ) {
+    return { rows: 0, columns: 0 };
+  }
+
+  if (containerWidth < maxItemWidth) {
+    const itemHeight = containerWidth / itemAspectRatio;
+    const rows = calculateMaxItemInBoxWithSpacing(
+      containerHeight,
+      itemHeight,
+      itemSpacing
+    );
+
+    return { rows, columns: 1 };
+  }
+
+  if (containerHeight < maxItemWidth / itemAspectRatio) {
+    const itemWidth = containerHeight * itemAspectRatio;
+    const columns = calculateMaxItemInBoxWithSpacing(
+      containerWidth,
+      itemWidth,
+      itemSpacing
+    );
+
+    return { rows: 1, columns };
+  }
+
+  const itemWidth = maxItemWidth;
+  const itemHeight = itemWidth / itemAspectRatio;
+
+  const rows = calculateMaxItemInBoxWithSpacing(
+    containerHeight,
+    itemHeight,
+    itemSpacing
+  );
+  const columns = calculateMaxItemInBoxWithSpacing(
+    containerWidth,
+    itemWidth,
+    itemSpacing
+  );
+
+  return { rows, columns };
+}
+
+const MainGridBox = () => {
+  const timeoutRef = React.useRef(null);
+
+  const [rows, setRows] = React.useState(3);
+  const [columns, setColumns] = React.useState(4);
+
+  const containerBoxRef = React.useCallback((node) => {
+    if (!node) return;
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        if (entry.contentRect) {
+          const { width, height } = entry.contentRect;
+          const padding = {
+            width: 8 * 2,
+            height: 8 * 2,
+          };
+
+          clearTimeout(timeoutRef.current);
+          timeoutRef.current = setTimeout(() => {
+            const { rows: rRows, columns: rColumns } =
+              calculateNumberOfItemsInGrid(
+                width - padding.width,
+                height - padding.height
+              );
+            setRows(rRows);
+            setColumns(rColumns);
+          }, 200);
+        }
+      }
+    });
+
+    resizeObserver.observe(node);
+  }, []);
+
+  return (
+    <div className="container-grid-box" ref={containerBoxRef}>
+      <div
+        className="grid-container"
+        style={{
+          "--item-max-width": `${maxItemWidth}px`,
+        }}
+      >
+        {Array(rows * columns)
+          .fill(0)
+          .map((item, index) => (
+            <GridItem />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="main-container">
+      <MainGridBox />
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
